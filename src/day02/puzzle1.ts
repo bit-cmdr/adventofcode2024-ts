9cc92af1ac1abd2780830832a4fd8a21e06ffb5f
Alexander Kahoun <alexander.kahoun@outlook.com>
2024-12-02 08:42:16 -0700

    feat: day 2 puzzle 1

---
+++ src/day02/puzzle1.ts
@@ -0,0 +1,39 @@
+import { puzzleInput } from '../tools.js';
+
+const reports = puzzleInput('src/day02/input.txt').split('\n');
+
+let safeCount = 0;
+
+for (const report of reports) {
+  const levels = report.split(' ').map(Number);
+  if (isSafeReport(levels)) {
+    safeCount++;
+  }
+}
+
+console.log(safeCount);
+
+function isSafeReport(report: number[]): boolean {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 1; i < report.length; i++) {
+    const l = report[i - 1];
+    const r = report[i];
+    if (!l || !r) {
+      continue;
+    }
+    const diff = l - r;
+    if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+      return false;
+    }
+    if (diff < 0) {
+      increasing = false;
+    }
+    if (diff > 0) {
+      decreasing = false;
+    }
+  }
+
+  return increasing || decreasing;
+}
