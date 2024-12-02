c0e4bcafa1970be6f47b9ca1eb6598fd6b3d9cfb
Alexander Kahoun <alexander.kahoun@outlook.com>
2024-12-02 08:24:11 -0700

    fix: optimized day 1 part 2

--- src/day01/puzzle2.ts
+++ src/day01/puzzle2.ts
@@ -5,37 +5,24 @@ const inputLines = puzzleInput('src/day01/input.txt').split('\n');
 const left: number[] = [];
 const right: number[] = [];
 for (const line of inputLines) {
-  const segments = line.split('   ');
-  if (segments.length !== 2) {
-    continue;
+  const [leftSegment, rightSegment] = line.split('   ');
+  if (leftSegment) {
+    left.push(toInt(leftSegment) ?? -1);
   }
-  if (segments[0]) {
-    left.push(toInt(segments[0]) ?? -1);
-  }
-  if (segments[1]) {
-    right.push(toInt(segments[1]) ?? -1);
+  if (rightSegment) {
+    right.push(toInt(rightSegment) ?? -1);
   }
 }
 
-const sortedLeft = left.toSorted();
-const sortedRight = right.toSorted();
+const sortedLeft = left.sort((a, b) => a - b);
+const sortedRight = right.sort((a, b) => a - b);
 const hash = new Map<number, number>();
-for (let i = 0; i < sortedRight.length; i++) {
-  const r = right[i];
-  if (!r) {
-    continue;
-  }
-
+for (const r of sortedRight) {
   hash.set(r, (hash.get(r) ?? 0) + 1);
 }
 
 const similarities: number[] = [];
-for (let i = 0; i < sortedLeft.length; i++) {
-  const left = sortedLeft[i];
-  if (!left) {
-    continue;
-  }
-
+for (const left of sortedLeft) {
   const similarity = left * (hash.get(left) ?? 0);
   similarities.push(similarity);
 }
